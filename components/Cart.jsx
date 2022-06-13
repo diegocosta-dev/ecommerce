d863a207e7b42772cbb66fef62c59165419d2fba
Diego Arthur Costa <troiablender@gmail.com>
2022-06-13 09:50:06 -0300

    created cart shopping

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Link from "next/link"
+import {
+  AiOutlineLeft,
+  AiOutlineShopping
+} from "react-icons/ai"
+
+import { useStateContext } from "../context/StateContext"
+
+
 
 const Cart = () => {
+  const cartRef = useRef()
+  const { 
+    totalPrice,
+    totalQuantity,
+    cartItems,
+    setShowCart
+  } = useStateContext()
+
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+        <button 
+        type='buttons'
+        className='cart-heading'
+        onClick={() => setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className='heading'>Your Cart</span>
+          <span className='cart-num-items'>({totalQuantity} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className='empty-cart'>
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button
+              type="button"
+              onClick={() => setShowCart(false)}
+              className="btn">
+                 Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
